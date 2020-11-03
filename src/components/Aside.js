87b65b2f46322d0b26342b50eff2d1a92c1bf9ce
Cristina Blanco <crisb995@gmail.com>
2020-11-02 22:32:38 -0700

    Sidemenu hover

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -48,14 +48,13 @@ const Aside = ({ image, collapsed, rtl, toggled, handleToggleSidebar, onChange }
         </Menu>
         <Menu iconShape="circle">
           <SubMenu title="Dashboard" >
-            <div class="menuoptioncontainer">
+
 
                <MenuItem onClick={()=>handleChange("Global Target Summary")}>Global Target Summary</MenuItem>
-            </div>
-           
+        
             <SubMenu icon title="Net Forest Cover Change">
-              <MenuItem onClick={()=>handleChange("Net Forest Cover Change 1")}>Net Forest Cover Change 1</MenuItem>
-              <MenuItem onClick={()=>handleChange("Net Forest Cover Change 2")}>Net Forest Cover Change 2</MenuItem>
+              <MenuItem class="submenu" onClick={()=>handleChange("Net Forest Cover Change 1")}>Net Forest Cover Change 1</MenuItem>
+              <MenuItem  class="submenu" onClick={()=>handleChange("Net Forest Cover Change 2")}>Net Forest Cover Change 2</MenuItem>
               {/*<SubMenu title="submenu">
                 <MenuItem>submenu 3.3.1 </MenuItem>
                 <MenuItem>submenu 3.3.2 </MenuItem>
@@ -66,26 +65,26 @@ const Aside = ({ image, collapsed, rtl, toggled, handleToggleSidebar, onChange }
             <MenuItem onClick={()=>handleChange("Protected Areas by Type")}>Protected Areas by Type</MenuItem>
             <MenuItem onClick={()=>handleChange("Land Cover")}>Land Cover</MenuItem>
             <SubMenu title="Fresh Water">
-              <MenuItem onClick={()=>handleChange("Fresh Water 1")}>Fresh Water 1</MenuItem>
-              <MenuItem onClick={()=>handleChange("Fresh Water 2")}>Fresh Water 2</MenuItem>
+              <MenuItem class="submenu" onClick={()=>handleChange("Fresh Water 1")}>Fresh Water 1</MenuItem>
+              <MenuItem  class="submenu"onClick={()=>handleChange("Fresh Water 2")}>Fresh Water 2</MenuItem>
             </SubMenu>
             <SubMenu title="Green House Gas (GHG) Emissions">
-              <MenuItem onClick={()=>handleChange("Green House Gas (GHG) Emissions 1")}>Green House Gas (GHG) Emissions 1</MenuItem>
-              <MenuItem onClick={()=>handleChange("Green House Gas (GHG) Emissions 2")}>Green House Gas (GHG) Emissions 2</MenuItem>
+              <MenuItem  class="submenu"onClick={()=>handleChange("Green House Gas (GHG) Emissions 1")}>Green House Gas (GHG) Emissions 1</MenuItem>
+              <MenuItem  class="submenu"onClick={()=>handleChange("Green House Gas (GHG) Emissions 2")}>Green House Gas (GHG) Emissions 2</MenuItem>
             </SubMenu>
             <SubMenu title="Food Energy Intake Per Capita">
-              <MenuItem onClick={()=>handleChange("Food Energy Intake Per Capita 1")}>Food Energy Intake Per Capita 1</MenuItem>
-              <MenuItem onClick={()=>handleChange("Food Energy Intake Per Capita 2")}>Food Energy Intake Per Capita 2</MenuItem>
+              <MenuItem  class="submenu"onClick={()=>handleChange("Food Energy Intake Per Capita 1")}>Food Energy Intake Per Capita 1</MenuItem>
+              <MenuItem  class="submenu"onClick={()=>handleChange("Food Energy Intake Per Capita 2")}>Food Energy Intake Per Capita 2</MenuItem>
             </SubMenu>
           </SubMenu>
             <SubMenu title="Trade Report">
               <SubMenu title="Sustainable - net">
-                <MenuItem onClick={()=>handleChange("Importers")}>Importers</MenuItem>
-                <MenuItem>Exporters</MenuItem>
+                <MenuItem class="submenu" onClick={()=>handleChange("Importers")}>Importers</MenuItem>
+                <MenuItem class="submenu" >Exporters</MenuItem>
               </SubMenu>
               <SubMenu title="Current Trend">
-                <MenuItem >Importers</MenuItem>
-                <MenuItem >Exporters</MenuItem>
+                <MenuItem class="submenu" >Importers</MenuItem>
+                <MenuItem  class="submenu">Exporters</MenuItem>
               </SubMenu>
 
             </SubMenu>
